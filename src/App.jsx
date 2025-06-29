bf23af85b4ad9b415a1faa086df1895634313db8
kelta-king <kushangshah131@gmail.com>
2025-06-29 12:20:14 +0530

    chartjs setup

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import ChartTypeSelector from "./components/ChartTypeSelector";
 import ValueTypeSelector from "./components/ValueTypeSelector";
 import RollingTypeSelector from "./components/RollingTypeSelector";
 import DurationSelector from "./components/DurationSelector";
-import ChartPlaceholder from "./components/ChartPlaceholder";
+import ChartPlaceholder, { generateChartData } from "./components/ChartPlaceholder";
 
 import PLACEHOLDERS from "./common/placeholders";
 import dataRepository from "./data/repository";
@@ -29,10 +29,10 @@ function App() {
 	async function showGrowthDirectFunds() {
 		const fundsList = await dataRepository.getAllDirectFundsList();
 		var funds = [];
-		fundsList.forEach(fund => {
+		fundsList.forEach((fund) => {
 			funds.push({
-				value: fund.code, 
-				label: fund.name
+				value: fund.code,
+				label: fund.name,
 			});
 		});
 		setFundOptions(funds);
@@ -41,20 +41,20 @@ function App() {
 	async function renderFirstData() {
 		const fundsList = await dataRepository.getAllDirectOrRegularFundsList();
 		var funds = [];
-		fundsList.forEach(fund => {
+		fundsList.forEach((fund) => {
 			funds.push({
-				value: fund.code, 
-				label: fund.name
+				value: fund.code,
+				label: fund.name,
 			});
 		});
 		setFundOptions(funds);
-		
+
 		const indexesList = await dataRepository.getAllIndexList();
 		var indexes = [];
-		indexesList.forEach(index => {
+		indexesList.forEach((index) => {
 			indexes.push({
 				value: index,
-				label: index
+				label: index,
 			});
 		});
 		setIndexOptions(indexes);
@@ -65,45 +65,61 @@ function App() {
 	}, []);
 
 	useEffect(() => {
-		if(showGrowthDirect) {
+		if (showGrowthDirect) {
 			showGrowthDirectFunds();
-		}
-		else {
+		} else {
 			renderFirstData();
 		}
 	}, [showGrowthDirect]);
+
+	const chartData = generateChartData();
 
 	return (
 		<div className="min-h-screen bg-gray-50 py-8 px-4 flex flex-col items-center">
 			<div className="max-w-2xl w-full bg-white rounded-lg shadow p-6">
 				<Header />
-				<SearchMutualFunds 
+				<SearchMutualFunds
 					fundOptions={fundOptions}
-					selectedMutualFunds={selectedMutualFunds} 
-					setSelectedMutualFunds={setSelectedMutualFunds} 
+					selectedMutualFunds={selectedMutualFunds}
+					setSelectedMutualFunds={setSelectedMutualFunds}
 				/>
-				<SearchIndexes 
+				<SearchIndexes
 					indexesOptions={indexOptions}
 					selectedIndexes={selectedIndexes}
 					setSelectedIndexes={setSelectedIndexes}
 				/>
-				<GrowthDirectCheckbox 
-					showGrowthDirect={showGrowthDirect} 
-					setShowGrowthDirect={setShowGrowthDirect} 
+				<GrowthDirectCheckbox
+					showGrowthDirect={showGrowthDirect}
+					setShowGrowthDirect={setShowGrowthDirect}
 				/>
-				<ChartTypeSelector chartType={chartType} setChartType={setChartType} />
-				<ValueTypeSelector valueType={valueType} setValueType={setValueType} />
-				<RollingTypeSelector rollingType={rollingType} setRollingType={setRollingType} />
-				<DurationSelector duration={duration} setDuration={setDuration} />
-				<button onClick={() => {
-					console.log(selectedIndexes);
-				}}>
+				<ChartTypeSelector
+					chartType={chartType}
+					setChartType={setChartType}
+				/>
+				<ValueTypeSelector
+					valueType={valueType}
+					setValueType={setValueType}
+				/>
+				<RollingTypeSelector
+					rollingType={rollingType}
+					setRollingType={setRollingType}
+				/>
+				<DurationSelector
+					duration={duration}
+					setDuration={setDuration}
+				/>
+				<button
+					onClick={() => {
+						console.log(selectedIndexes);
+					}}
+				>
 					{"click"}
 				</button>
-				<ChartPlaceholder />
+				<ChartPlaceholder data={chartData} maxLines={5} />
 			</div>
 		</div>
 	);
 }
+
 
 export default App;
